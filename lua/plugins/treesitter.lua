1a7a1fc31d60c22a43be5a9054bcc59df8356c6b
maskduck <maskduck@proton.me>
2025-10-05 22:15:37 +0700

    I'm stupid

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,5 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
-	lazy = vim.fn.argc(-1) == 0,
-	cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
 	opts = {
 		ensure_installed = { "c", "python", "arduino" },
 		auto_install = true
