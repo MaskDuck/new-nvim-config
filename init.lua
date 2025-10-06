38a99b673656c369c3f8ce8eb338b0cd98581345
maskduck <maskduck@proton.me>
2025-10-06 18:38:59 +0700

    Add image rendering tester

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require 'misc.gnopts'
+require 'misc.disable-mouse'
 require 'misc.keymaps'
 require 'misc.prestart'
 require 'misc.lazy'
---
+++ lua/misc/disable-mouse.lua
@@ -0,0 +1,2 @@
+vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<Down>', function() end, { desc = "disable mouse" })
+vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<Up>', function() end, { desc = "disable mouse" })
